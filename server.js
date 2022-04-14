a5f16db7c02a3274f465078491aa13ce6d954d4b
dinushchathurya <dinushchathurya21@gmail.com>
2022-04-14 11:56:21 +0530

    :art: Add trigger branches

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world Azure\n');
+  res.send('Hello world Azure Container\n');
 });
 
 app.listen(PORT, HOST);
